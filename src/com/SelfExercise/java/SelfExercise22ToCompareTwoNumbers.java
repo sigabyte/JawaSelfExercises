a7dfafd71057caafd87c3a40af4c08f447f0beb4
sigabyte <ibrahimciga@hotmail.com>
2021-09-11 13:49:44 +0100

    New file added

---
+++ src/com/SelfExercise/java/SelfExercise22ToCompareTwoNumbers.java
@@ -0,0 +1,23 @@
+package com.SelfExercise.java;
+
+public class SelfExercise22ToCompareTwoNumbers {
+	public static void main(String[] args) {
+
+		int num1 = 22, num2 = 87;
+
+		if (num1 != num2) {
+			System.out.println(num1 + "!=" + num2);
+		}
+		if (num1 == num2) {
+			System.out.println(num1 + "=" + num2);
+		}
+		if (num1 > num2) {
+			System.out.println(num1 + ">" + num2);
+		}
+		if (num1 < num2) {
+			System.out.println(num1 + "<" + num2);
+		}
+
+	}
+
+}
